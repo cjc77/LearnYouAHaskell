350ce9ca05aa03602295755a6b9cec1d33b75f22
cjc77 <carsonjamescook@gmail.com>
2017-05-01 16:38:38 -0700

    gleelkj

--- Ch2/ListComprehensions.hs
+++ Ch2/ListComprehensions.hs
@@ -6,3 +6,10 @@ mult2Under10' = [x * 2 | x <- [1..10], x * 2 >= 12]
 
 -- Complicated comprehension
 pickyList = [x | x <- [1..100], x `mod` 7 == 3]
+
+
+boomBangs xs = [ if x < 10 then "Boom!" else "Bang!" | x <- xs, odd x ]
+
+anotherPickyList = [x | x <- [10..20], x /= 13, x /= 15, x /= 19]
+
+cartesian = [ x * y | x <- [2, 5, 10], y <- [8, 10, 11]]
