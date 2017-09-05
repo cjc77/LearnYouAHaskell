919589932d539c9adff95a5151dbdd74bd879abb
carson <carsonjamescook@gmail.com>
2017-09-04 19:04:03 -0700

    4.4

---
+++ Ch4/4.4_Let.hs
@@ -0,0 +1,23 @@
+
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^ 2
+    in sideArea + 2 * topArea
+
+
+squareTriple :: (Num a) => a -> a -> a -> (a,a,a)
+squareTriple a b c =
+    let (x,y,z) = (a,b,c) in (x*x,y*y,z*z)
+
+calcBMis :: (RealFloat a) => [(a,a)] -> [a]
+calcBMis xs = [bmi | (w,h) <- xs, let bmi = w / h ^ 2]
+
+
+-- Check list of pairs to see if some pairs are repeats of others
+determineSame :: (Eq a) => [(a,a)] -> [Bool]
+determineSame xs = [eq | (x,y) <- xs, let eq = (x == y)]
+
+pairSumAbove100 :: (Ord a, Num a) => [(a,a)] -> [(a,a)]
+pairSumAbove100 xs = [(x,y) | (x,y) <- xs, let pairSum = x + y, pairSum > 100]
