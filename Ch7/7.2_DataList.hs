145e2904a7d485ec3180eac385599930a6c50c36
carson <carsonjamescook@gmail.com>
2017-09-10 23:01:05 -0700

    Done with Data.List section.

--- Ch7/7.2_DataList.hs
+++ Ch7/7.2_DataList.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.Function (on)
 
 addPolynomials :: (Num b) => [[b]] -> [b]
 addPolynomials xs = map sum $ transpose xs
@@ -8,3 +9,10 @@ findSublist :: (Eq a) => [a] -> [a] -> Bool
 findSublist target list =
     let tLen = length target
     in foldl (\acc x -> if take tLen x == target then True else acc) False (tails list)
+
+clusterPositives :: (Ord a, Num a) => [a] -> [[a]]
+clusterPositives xs = groupBy ((==) `on` (>0)) xs
+
+
+sortByLength :: (Foldable t) => [t a] -> [t a]
+sortByLength xs = sortBy (compare `on` length) xs
