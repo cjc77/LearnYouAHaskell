85d226e74518401e52b2432cb49cc8e6d4a0d5ff
carson <carsonjamescook@gmail.com>
2017-09-11 20:39:09 -0700

    Done with 8.1

--- Ch8/8.1_AlgebraicDataTypes.hs
+++ Ch8/8.1_AlgebraicDataTypes.hs
@@ -10,3 +10,8 @@ surfaceArea :: Shape -> Float
 surfaceArea (Circle _ r) = pi * r ^ 2
 surfaceArea (Rectangle (Point x1 y1) (Point x2 y2)) =
             (abs $ x2 - x1) * (abs $ y2 - y1)
+
+move :: Shape -> Float -> Float -> Shape
+move (Circle (Point x y) r) a b = Circle (Point (x+a) (y+b)) r
+move (Rectangle (Point x1 y1) (Point x2 y2)) a b =
+            Rectangle (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))
