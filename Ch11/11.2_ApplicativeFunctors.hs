3f63465021411b6dc49fb4566164ff089321838a
carson <carsonjamescook@gmail.com>
2017-09-21 21:54:42 -0700

    Done with 11.2

--- Ch11/11.2_ApplicativeFunctors.hs
+++ Ch11/11.2_ApplicativeFunctors.hs
@@ -1,3 +1,4 @@
+import Control.Applicative
 
 productsGTEN :: (Ord a, Num a) => a -> [a] -> [a] -> [a]
 productsGTEN n xs ys = filter (>n) $ (*) <$> xs <*> ys
@@ -13,3 +14,14 @@ productsGTEN n xs ys = filter (>n) $ (*) <$> xs <*> ys
 -- Method 2 (applicative style)
 myAction :: IO String
 myAction = (++) <$> getLine <*> getLine
+
+
+
+-- sequenceA :: (Applicative f) => [f a] -> f [a]
+-- sequenceA [] = pure []
+-- sequenceA (x:xs) = (:) <$> x <*> sequenceA xs
+
+
+-- Using a fold
+-- sequenceA :: (Applicative f) => [f a] -> f [a]
+-- sequenceA = foldr (liftA2 (:)) (pure [])
